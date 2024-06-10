c4b6466790a4db788d5d318d49dd84baebca6a34
MoranKa <moran@komodor.io>
2024-06-10 16:48:10 +0300

    chore: refactor example files

--- examples/main.tf
+++ examples/main.tf
@@ -1,16 +1,3 @@
-terraform {
-  required_providers {
-    komodor = {
-      version = ">= 1.0.6"
-      source  = "komodorio/komodor"
-    }
-  }
-}
-
-provider "komodor" {
-  api_key = var.api_key
-}
-
 resource "komodor_policy" "my-policy" {
   name       = "my-policy"
   statements = <<EOF
@@ -40,91 +27,6 @@ resource "komodor_policy_role_attachment" "my-attachement" {
   name     = "test-attachement"
   policies = [komodor_policy.my-policy.id]
   role     = komodor_role.my-role.id
-}
-
-resource "komodor_monitor" "example-deploy-monitor" {
-  name          = "example-deploy-monitor"
-  type          = "deploy"
-  active        = true
-  sensors       = <<EOF
-[{
-  "cluster": "kind-kind",
-  "exclude": {
-    "namespaces": ["komodor"]
-  },
-  "namespaces": [
-    "default"
-  ]
-}]
-EOF
-  sinks         = <<EOF
-{
-  "slack": [
-    "default"
-  ],
-  "teams": [
-    "default"
-  ],
-  "pagerduty": [{
-    "channel": "example-channel",
-    "integrationKey": "example-pagerduty-integration-key",
-    "pagerDutyAccountName": "example-pagerduty-account-name"
-  }]
-}
-EOF 
-  sinks_options = <<EOF
-{
-  "notifyOn": ["Failure"]
-}
-EOF 
-}
-
-resource "komodor_monitor" "example-availability-monitor" {
-  name      = "example-availability-monitor"
-  type      = "availability"
-  active    = true
-  sensors   = <<EOF
-[{
-  "cluster": "kind-kind",
-  "exclude": {
-    "services": ["default/service-to-exclude"]
-  },
-  "services": [
-    "default/service-to-include"
-  ],
-  "condition": "and",
-  "namespaces": ["default"]
-}]
-EOF
-  sinks     = <<EOF
-{
-  "slack": [
-    "default"
-  ],
-  "teams": [
-    "default"
-  ],
-  "pagerduty": [{
-    "channel": "example-channel",
-    "integrationKey": "example-pagerduty-integration-key",
-    "pagerDutyAccountName": "example-pagerduty-account-name"
-  }]
-}
-EOF  
-  variables = <<EOF
-{
-  "categories": [
-    "*"
-  ],
-  "duration": 30,
-  "minAvailable": "100%"
-}
-EOF 
-  sinks_options = <<EOF
-{
-  "notifyOn": ["*"]
-}
-EOF 
 }
 
 data "komodor_policy" "my-policy" {
---
+++ examples/policy_with_action.tf
@@ -0,0 +1,38 @@
+resource "komodor_action" "komo-example-pod-viewer" {
+  action       = "pod-viewer"
+  description = "View pods"
+  ruleset = <<EOF
+[
+  {
+    "apiGroups": [
+      "apps"
+    ],
+    "resources": [
+      "pods"
+    ],
+    "verbs": [
+      "get",
+      "list"
+    ]
+  }
+]
+EOF
+}
+
+resource "komodor_policy" "komo-example-policy" {
+  name       = "komo-example-policy"
+  statements = <<EOF
+[{
+  "actions": [
+    "${komodor_action.komo-example-pod-viewer.action}"
+  ],
+  "resources": [{
+    "cluster": "komo-example-cluster",
+    "namespaces": [
+      "default",
+      "kube-system"
+    ]
+  }]
+}]
+EOF
+}
