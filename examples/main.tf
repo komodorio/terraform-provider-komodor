9e7df8744e47f006edacb1b3b144f724fcbf0ac5
MoranKa <moran@komodor.io>
2026-04-06 10:57:22 +0300

    update example

--- examples/main.tf
+++ examples/main.tf
@@ -2,12 +2,20 @@ resource "komodor_role" "my-role" {
   name = "my-role"
 }
 
+resource "komodor_policy_v2" "my-policy" {
+  name = "view-all"
+
+  statements {
+    actions = ["view:all"]
+
+    resources_scope {
+      clusters   = ["management"]
+      namespaces = ["default", "komodor"]
+    }
+  }
+
 resource "komodor_policy_role_attachment" "my-attachement" {
   name     = "test-attachement"
-  policies = []
+  policies = [komodor_policy_v2.my-policy.id]
   role     = komodor_role.my-role.id
-}
-
-data "komodor_policy_v2" "my-policy" {
-  name = "default-read-only"
 }
